2d1ebc6da2e1098d657a0511df719d522c8befa6
Brian Robinson <br7pc@mail.missouri.edu>
2019-11-08 16:19:45 -0600

    first commit

---
+++ Inventory/Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Brian Robinson on 11/8/19.
+//  Copyright © 2019 Brian Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
